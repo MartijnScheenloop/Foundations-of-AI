c7d76d543ca4b4f2c5786e980e681b3526b39007
Martijn <m.scheenloop@hotmail.com>
2022-11-29 23:46:38 +0100

    Added check of section in legal function

--- team38_A1/sudokuai.py
+++ team38_A1/sudokuai.py
@@ -9,7 +9,7 @@ import competitive_sudoku.sudokuai
 
 # Check if numpy can be used!
 import numpy as np
-
+import math
 
 class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     """
@@ -35,55 +35,33 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         
         columns = np.transpose(rows)
 
-        # !!! CREATE A LIST OF THE BOARD'S SECTIONS AND ADD THIS CHECK TO THE LEGAL FUNCTION BELOW !!!
-
-        # Determine if a certain move is legal in a certain gamestate
-        def legal(i, j, value):
-            return not value in rows[i] and not value in columns[j]
+        # Determine if action is legal (not already present in section, row or column)
+        def legal(i,j,value,data):
+            size = int(np.sqrt(len(rows)))
+            row = math.floor(i / size)
+            col = math.floor(j/size)
+            y= np.vstack([xi for xi in data])
+            return not value in np.array(y[row*size:row*size+size,col*size:col*size+size]).reshape(-1,).tolist() and not value in rows[i] and not value in columns[j]
    
-        # Compute a list of all possible (non-taboo AND legal) moves for a certain gamestate
-        legal_moves = [Move(i, j, value) for i in range(N) for j in range(N)
-                        for value in range(1, N+1) if legal(i, j, value)]
-
         non_taboo_moves = []
         possible_moves = []
+
         for i in range(N):
             for j in range(N):
                 for value in range(1, N+1):
                     if non_taboo(i, j, value):
                         non_taboo_moves.append(Move(i, j, value))
-                        if legal(i, j, value):
+                        if legal(i, j, value, rows):
                             possible_moves.append(Move(i, j, value))
+                            
 
-        # legal_moves = []
-        # for i in range(N):
-        #     for j in range(N):
-        #         for value in range(1, N+1):
-        #             if legal(i, j, value):
-        #                 legal_moves.append(Move(i, j, value))
-            
-        # Create a list of the boards resulting from the possible moves
-        # possible_boards = []
-        # current_board = game_state.board
-
-        print('LEGAL MOVES:')
-        for move in legal_moves:
-            print(move)
         print('NON-TABOO MOVES:')
         for move in non_taboo_moves:
             print(move)
         print('POSSIBLE MOVES (LEGAL AND NON-TABOO):')
         for move in possible_moves:
             print(move)
-        #     (i, j, value) = (move.i, move.j, move.value)
-        #     current_board.put(i, j, value)
-        #     board = current_board
-        #     possible_boards.append(board)
-        
-        # for board in possible_boards:
-        #     print_board(board)
 
-        # Pick a random move from the list of possible moves
         move = random.choice(possible_moves)
         self.propose_move(move)
 
