c866859c34619908f9ddec6a102bba988fe1eb61
20174216 <j.w.j.linders@student.tue.nl>
2022-11-30 22:56:09 +0100

    Score function working

--- team38_A1/sudokuai.py
+++ team38_A1/sudokuai.py
@@ -92,16 +92,18 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         # current_rows = []
         # for i in range(N):
         #     current_rows.append(current_board[i*N : (i+1)*N])
-        print('Current rows:', rows)
-        print('Current columns:', columns)
+        # print('Current rows:', rows)
+        # print('Current columns:', columns)
         # print(current_rows)
         # print(current_columns)
         current_rows = deepcopy(rows)
         current_columns = deepcopy(columns)
+        best_move = Move(0, 0, 0)
+        best_count = 0
 
         for move in possible_moves:
-            print("Current row:   ", current_rows[move.i])
-            print("Current column:", current_columns[move.j])
+            # print("Current row:   ", current_rows[move.i])
+            # print("Current column:", current_columns[move.j])
             current_row_complete = not 0 in current_rows[move.i]
             current_column_complete = not 0 in current_columns[move.j]
 
@@ -110,8 +112,8 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
             new_rows[move.i][move.j] = move.value
             new_columns = np.transpose(new_rows)
 
-            print("New row:       ", new_rows[move.i])
-            print("New column:    ", new_columns[move.j])
+            # print("New row:       ", new_rows[move.i])
+            # print("New column:    ", new_columns[move.j])
             new_row_complete = not 0 in new_rows[move.i]
             new_column_complete = not 0 in new_columns[move.j]
             
@@ -123,12 +125,28 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
             # if section_complete:
             #     count +=1
             # print('Final count is:', count, '\n')
-            print('Current row complete:   ', current_row_complete)
-            print('Current column complete:', current_column_complete)
-            print('New row complete:       ', new_row_complete)
-            print('New column complete:    ', new_column_complete, '\n')
-            print('Count:', count)
+            # print('Current row complete:   ', current_row_complete)
+            # print('Current column complete:', current_column_complete)
+            # print('New row complete:       ', new_row_complete)
+            # print('New column complete:    ', new_column_complete, '\n')
+            # print('Count:', count)
 
+            size_row = math.floor(np.sqrt(len(current_rows)))
+            size_col = math.ceil(np.sqrt(len(current_rows)))
+            row = math.floor(move.i/size_row)
+            col = math.floor(move.j/size_col) 
+            y= np.vstack([xi for xi in current_rows])           
+            current_section = np.array(y[row*size_row:row*size_row+size_row,col*size_col:col*size_col+size_col]).reshape(-1,).tolist()
+            # print('Current section list:', current_section)
+                    
+            if current_section.count(0) == 1:
+                count += 1
+            
+            print('Count =', count)
+
+            if count > best_count:
+                best_count = count
+                self.propose_move(move)
 
         # print('CURRENT SCORE')
         # for move in game_state.scores:
@@ -137,10 +155,10 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         # for move in game_state.moves:
         #     print(move)
 
-        move = random.choice(possible_moves)
-        self.propose_move(move)
+        # move = random.choice(possible_moves)
+        # self.propose_move(move)
 
-        # Randomly repick the move from the list every 0.2 seconds until the time runs out
-        while True:
-            time.sleep(0.2)
-            self.propose_move(random.choice(possible_moves))
+        # # Randomly repick the move from the list every 0.2 seconds until the time runs out
+        # while True:
+        #     time.sleep(0.2)
+        #     self.propose_move(random.choice(possible_moves))
